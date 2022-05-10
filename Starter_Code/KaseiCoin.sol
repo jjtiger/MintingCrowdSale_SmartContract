aaac938493471eec3074f2ca3e6419537b1bd542
jjtiger <91930868+jjtiger@users.noreply.github.com>
2022-05-09 21:51:03 -0700

    Second Push

--- Starter_Code/KaseiCoin.sol
+++ Starter_Code/KaseiCoin.sol
@@ -1,10 +1,22 @@
+
+//  Import the following contracts from the OpenZeppelin library:
+//    * `ERC20`
+//    * `ERC20Detailed`
+//    * `ERC20Mintable`
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20Detailed.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20Mintable.sol";
+
+// Create a constructor for the KaseiCoin contract and have the contract inherit the libraries that you imported from OpenZeppelin.
+
+
 pragma solidity ^0.5.0;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20Detailed.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20Mintable.sol";
 
-contract Arcade Token is ERC20, ERC20Detailed {
+contract KaseiCoin is ERC20, ERC20Detailed {
     address payable owner;
     
     modifier onlyOwner {
@@ -13,7 +25,7 @@ contract Arcade Token is ERC20, ERC20Detailed {
     }
 
 
-    constructor(uint initial_supply) ERC20Detailed("ArcadeToken", "ARCD", 18) public {
+    constructor(uint initial_supply) ERC20Detailed("KaseiToken", "KCC", 18) public {
         owner = msg.sender;
         _mint(owner, intitial_supply);
     }
